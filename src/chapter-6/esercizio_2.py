d63c50846a4fdd06342a7638df4cda5b57524ea3
francesco finucci <francesco.finucci@accenture.com>
2023-07-01 15:26:32 +0200

    Ended chapter 6

--- src/chapter-6/esercizio_2.py
+++ src/chapter-6/esercizio_2.py
@@ -23,7 +23,26 @@ print(GDP_per_capita)
 alco_over_mean_value = alco_per_capita["Total"] > alco_per_capita["Total"].mean()
 print(alco_over_mean_value)
 GDP_over_mean_value = GDP_per_capita['CIA[8][9][10]']['Estimate'] > GDP_per_capita['CIA[8][9][10]']['Estimate'].mean()
-print(GDP_per_capita['CIA[8][9][10]']['Estimate'].mean())
 
-cross_table_over = pd.crosstab(GDP_over_mean_value, alco_over_mean_value)
-print(cross_table_over)
+cross_table_over = pd.crosstab(GDP_over_mean_value, alco_over_mean_value).unstack()
+
+
+data = pd.DataFrame({"under_alco_num": cross_table_over.groupby("Total").sum().iloc[0],
+                     "over_alco_num": cross_table_over.groupby("Total").sum().iloc[1],
+                     "over_alco_over_GDP": cross_table_over.iloc[3],
+                     "over_alco_under_GDP": cross_table_over.iloc[2],
+                     "under_alco_over_GDP": cross_table_over.iloc[1],
+                     "under_alco_under_GDP": cross_table_over.iloc[0]},
+                     index=["Val"])
+print(data)
+print("\n----ANALISI STATI CON UN CONSUMO DI ALCOOL SUPERIORE ALLA MEDIA----")
+print("Percentuale di stati sopra alla media per consumo di alcool che hanno GDP sopra alla media:")
+print(str(data["over_alco_over_GDP"].values[0] / data["over_alco_num"].values[0] * 100) + "%")
+print("Percentuale di stati sopra alla media per consumo di alcool che hanno GDP sotto alla media:")
+print(str(data["over_alco_under_GDP"].values[0] / data["over_alco_num"].values[0] * 100) + "%")
+
+print("\n----ANALISI STATI CON UN CONSUMO DI ALCOOL INFERIORE ALLA MEDIA----")
+print("Percentuale di stati sotto alla media per consumo di alcool che hanno GDP sopra alla media:")
+print(str(data["under_alco_over_GDP"].values[0] / data["under_alco_num"].values[0] * 100) + "%")
+print("Percentuale di stati sotto alla media per consumo di alcool che hanno GDP sotto alla media:")
+print(str(data["under_alco_under_GDP"].values[0] / data["under_alco_num"].values[0] * 100) + "%")
